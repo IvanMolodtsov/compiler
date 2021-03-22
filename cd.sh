1bbfa739bae4af9e40c67dc2839f8646571d1073
IMolodtsov <vanmolodtsov@gmail.com.com>
2021-03-22 14:58:53 +0600

    dane

--- cd.sh
+++ cd.sh
@@ -1,1 +1,5 @@
-cd /mnt/c/Users/vanmo/Desktop/compiler
+#cd /mnt/c/Users/vanmo/Desktop/compiler
+gcc src/main.c -o sample.out -lusb-1.0
+sudo chown root:root sample.out
+sudo chmod u+s sample.out
+./sample.out
--- src/main.c
+++ src/main.c
@@ -1,22 +1,48 @@
-#include "include/ptr.h"
-#include "include/object.h"
+// #include "include/ptr.h"
+// #include "include/object.h"
+#include "stdlib.h"
+#include <libusb-1.0/libusb.h>
+#include <stdio.h>
 
-void main() {
-    ptr* a = smalloc(sizeof(int), NULL);
-    int val = 5;
-    ptr_set(a, &val);
-    printf("%i\n",a->size);
-    printf("%i\n",*((int *)a->to));
-    object* o = new_object(10);
+int main() {
+    // ptr* a = smalloc(sizeof(int), NULL);
+    // int val = 5;
+    // ptr_set(a, &val);
+    // printf("%i\n",a->size);
+    // printf("%i\n",*((int *)a->to));
+    // object* o = new_object(10);
 
-    set(o,"key1", a);
+    // set(o,"key1", a);
 
-    ptr* b = get(o, "key1");
-    printf("%i\n",b->size);
+    // ptr* b = get(o, "key1");
+    // printf("%i\n",b->size);
 
-    del(o);
-    printf("%i\n",o->size);
-    ptr* c = get(o, "key1");
-    printf("end");
-
+    // del(o);
+    // printf("%i\n",o->size);
+    // ptr* c = get(o, "key1");
+    // printf("end");
+    // int VID = 951;
+    // int PID = 1665;
+    int res = libusb_init(NULL);  
+    if (res != 0) {
+        printf("Error initialising libusb.\n");
+        return 1;
+    } 
+    // инициализация
+    libusb_device_handle *handle = libusb_open_device_with_vid_pid(NULL, 0x0951, 0x1665);
+    if (handle == NULL) {
+        printf("access denied\n");
+    } else {
+        printf("DA\n");
+    }
+    // libusb_device **devList = NULL;
+    // int numUsbDevs = libusb_get_device_list (NULL, &devList);
+    // for(int i=0; i< numUsbDevs; i++) {
+    //     libusb_device *dev = devList[i];
+    //     libusb_device_handle *handle = NULL;
+    //     int retVal = libusb_open (dev, &handle);
+    //     if (retVal != LIBUSB_SUCCESS)
+    //         printf("error");
+        
+    // }
 }
